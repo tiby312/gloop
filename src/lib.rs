793db71dec0274cf9e6b234968046371a7155d35
tiby312 <kenakioreed@gmail.com>
2025-02-15 20:57:50 -0500

    n

--- src/lib.rs
+++ src/lib.rs
@@ -21,29 +21,21 @@ pub struct FnWrapper<F>(F);
 
 
 
-
-
-// pub fn from_closure<F:FnMut(&Event),S>(target:&EventTarget,event_type:S,func:F)->EventListenerWrapper<impl Listen> where
-// S: Into<Cow<'static, str>>,{
-//     EventListenerWrapper::new(target,event_type,func)
-// }
-
-
-pub struct EventListenerWrapper<F>{
+pub struct EventListen<F>{
     _a:EventListener,
     func:std::marker::PhantomData<F>
     
 }
 
-impl<F:FnMut(&Event)> EventListenerWrapper<FnWrapper<F>>{
+impl<F:FnMut(&Event)> EventListen<FnWrapper<F>>{
 
-    pub fn from_closure<S>(target:&EventTarget,event_type:S,func:F)->EventListenerWrapper<FnWrapper<F>>
+    pub fn from_closure<S>(target:&EventTarget,event_type:S,func:F)->EventListen<FnWrapper<F>>
      where S:Into<Cow<'static,str>>{
-        EventListenerWrapper::new(target,event_type,FnWrapper(func))
+        EventListen::new(target,event_type,FnWrapper(func))
     }
 }
 
-impl<F: Listen> EventListenerWrapper<F>{
+impl<F: Listen> EventListen<F>{
     pub fn new<S>(target: &EventTarget, event_type: S, func: F) -> Self
 where
     S: Into<Cow<'static, str>>,
@@ -58,38 +50,6 @@ where
 
         let e=EventListener::new_with_options(target,event_type,options,move|e|callback.call(e));
 
-        EventListenerWrapper { _a: e, func:std::marker::PhantomData }
+        EventListen { _a: e, func:std::marker::PhantomData }
     }
 }
-
-
-// pub struct EventListenerWrapper2<F>{
-//     _a:EventListener,
-//     func:std::marker::PhantomData<F>
-    
-// }
-
-
-// impl<F: FnMut(&Event)> EventListenerWrapper2<F>{
-//     pub fn new<S>(target: &EventTarget, event_type: S, func: F) -> Self
-// where
-//     S: Into<Cow<'static, str>>,
-//     {
-//         let callback:Box<dyn FnMut(&Event)>=Box::new(func);
-//         let j=Box::into_raw(callback);
-//         let j:*mut (dyn FnMut(&Event)+'static)=j as *mut _;
-
-        
-//         let mut callback:Box<dyn FnMut(&Event)+'static>=unsafe{Box::from_raw(j)};
-
-//         let options = gloo::events::EventListenerOptions::enable_prevent_default();
-
-//         //EventListener::new_with_options(target, event_type, options, callback)
-
-//         let e=EventListener::new_with_options(target,event_type,options,move|e|callback.call(e));
-
-//         EventListenerWrapper2 { _a: e, func:std::marker::PhantomData }
-//     }
-// }
-
-
